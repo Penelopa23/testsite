05ac504154f773262c957fc72eb56fcfef5de6e2
Artem <timoseyka@gmail.com>
2022-11-02 21:37:01 +0400

    fixed cost

--- bucket.js
+++ bucket.js
@@ -37,7 +37,7 @@ function setBill(name, amount) {
 }
 
 //Форматируем общую стоимость чтобы не было кучи нулей после запятой
-mainCost = new Intl.NumberFormat("en", {style: "currency", currency: "USD", maximumFractionDigits: 2}).format(cost);
+mainCost = cost;
 //Вставлем общую стоимость в маапу чтобы потом передать в бота
 bucket.set("orderPrice", mainCost);
 tg.MainButton.setText("Pay " + mainCost); //Вставляем техт в кнопку
