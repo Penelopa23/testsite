c211df1c2f161efe0be34e7dee80cb14fcfe898a
Penelopa23 <40712622+Penelopa23@users.noreply.github.com>
2022-12-23 00:35:53 +0300

    Update send.js

--- send.js
+++ send.js
@@ -11,34 +11,40 @@ tg.MainButton.setText("SEND") //  метод для задания текста
 tg.MainButton.show() // показать кнопку 
 // tg.MainButton.hide() // скрыть кнопку
 // tg.MainButton.disable() // сделать неактивной 
-console.log(tg.initDataUnsafe)
 
-// console.log(tg.initDataUnsafe.user)
 tg.MainButton.show(); //Показываем кнопку
-
+if(tg.initDataUnsafe.user.id == 179755741) {
     tg.MainButton.enable() // сделать активной 
-
+}else {
+    tg.MainButton.disable()
+}
+console.log(tg.initDataUnsafe.user.id) // уникальный идентификатор пользователя
+console.log(tg.initDataUnsafe.user.isBot) // бот ли пользователь (true/false)
+console.log(tg.initDataUnsafe.user.first_name) // имя пользователя
+console.log(tg.initDataUnsafe.user.last_name) // "фамилия" пользователя
+console.log(tg.initDataUnsafe.user.username) // username пользователя
+console.log(tg.initDataUnsafe.user.language_code) // код языка пользователя
 
 //Отправляем данные о выбранном товаре
 Telegram.WebApp.onEvent('mainButtonClicked', function(){
     var wallet = document.getElementById('wallet');
     var sum = document.getElementById('sum');
-    let message = sum + ":" + wallet + ":" + tg.initDataUnsafe.user.id;
-    answerWebAppQuery(message);
+    tg.sendData(sum + ":" + wallet + ":" + tg.initDataUnsafe.user.id);
+    // answerWebAppQuery(message);
  });
  
- async function answerWebAppQuery(message) {
-   //Создаём запрос
-    let url = 'https://api.telegram.org/bot5558689984:AAHktTbnkTXsBAdPX59CuBeqYC1gkmUC2pE/answerWebAppQuery?web_app_query_id=' + 
-                tg.initDataUnsafe.query_id + '&result={"type":"article","id":123,"title":"123","message_text":"'+ message +'"}'
-    console.log(url);
-    //Отправляем данные в бота
-    fetchAsync(url);
- }
+//  async function answerWebAppQuery(message) {
+//    //Создаём запрос
+//     let url = 'https://api.telegram.org/bot5558689984:AAHktTbnkTXsBAdPX59CuBeqYC1gkmUC2pE/answerWebAppQuery?web_app_query_id=' + 
+//                 tg.initDataUnsafe.query_id + '&result={"type":"article","id":123,"title":"123","message_text":"'+ message +'"}'
+//     console.log(url);
+//     //Отправляем данные в бота
+//     fetchAsync(url);
+//  }
 
- //Метод отправкии сообщения боту
- async function fetchAsync (url) {
-   fetch(url)
-     .then(res => res.json())
-     .then(json => console.log(json));
+//  //Метод отправкии сообщения боту
+//  async function fetchAsync (url) {
+//    fetch(url)
+//      .then(res => res.json())
+//      .then(json => console.log(json));
 }
