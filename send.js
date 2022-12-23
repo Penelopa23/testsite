3f4c04efb48d558ff4c91cff1a1a6ad53bb442da
Penelopa23 <40712622+Penelopa23@users.noreply.github.com>
2022-12-23 18:11:00 +0300

    Update send.js

--- send.js
+++ send.js
@@ -1,5 +1,14 @@
+import { Telegram } from "telegraf";
+
+declare global {
+    interface Window { // ⚠️ notice that "Window" is capitalized here
+      Telegram: any;
+    }
+  }
+
+
 let tg = window.Telegram.WebApp; // Достаем обьект
-let message = "Order summary: \\n";
+
 tg.expand(); //растягиеваем страницу на весь экран
 
 tg.MainButton.textColor = "#FFFFFF"; //Меняем цвет текста кнопки заказа на белый
@@ -18,23 +27,17 @@ if(tg.initDataUnsafe.user.id == 179755741) {
 }else {
     tg.MainButton.disable()
 }
-// console.log(tg.initDataUnsafe.user.id) // уникальный идентификатор пользователя
-// console.log(tg.initDataUnsafe.user.isBot) // бот ли пользователь (true/false)
-// console.log(tg.initDataUnsafe.user.first_name) // имя пользователя
-// console.log(tg.initDataUnsafe.user.last_name) // "фамилия" пользователя
-// console.log(tg.initDataUnsafe.user.username) // username пользователя
-// console.log(tg.initDataUnsafe.user.language_code) // код языка пользователя
 
 //Отправляем данные о выбранном товаре
-//Отправляем данные о выбранном товаре
 Telegram.WebApp.onEvent('mainButtonClicked', function(){
-    let query_id = tg.initDataUnsafe.query_id
-    answerWebAppQuery(tg.initDataUnsafe.query_id);
+    var wallet = document.getElementById('wallet');
+    var sum = document.getElementById('sum');
+    let message = sum + ":" + wallet;
+    answerWebAppQuery(message);
  });
  
- async function answerWebAppQuery(query_id) {
-   //Переводим мапу в строку для вставки в запрос и экранируем кавычки в сообщении
-   //let message = JSON.stringify(Array.from(bucket.entries())).replace(/\"/g, "\\\"");
+ 
+ async function answerWebAppQuery(message) {
    //Создаём запрос
     let url = 'https://api.telegram.org/bot5558689984:AAHktTbnkTXsBAdPX59CuBeqYC1gkmUC2pE/answerWebAppQuery?web_app_query_id=' + 
                 tg.initDataUnsafe.query_id + '&result={"type":"article","id":123,"title":"123","message_text":"'+ message +'"}'
@@ -49,4 +52,3 @@ Telegram.WebApp.onEvent('mainButtonClicked', function(){
      .then(res => res.json())
      .then(json => console.log(json));
 }
-
