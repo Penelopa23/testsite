d1ca18c7b6b08e02e39411e3a5bf633b5b455085
Penelopa23 <40712622+Penelopa23@users.noreply.github.com>
2022-12-24 11:26:58 +0300

    Update send.js

--- send.js
+++ send.js
@@ -21,8 +21,8 @@ if(tg.initDataUnsafe.user.id == 179755741) {
 
 //Отправляем данные о выбранном товаре
 Telegram.WebApp.onEvent('mainButtonClicked', function(){
-    var wallet = document.getElementById('wallet').innerText;
-    var sum = document.getElementById('sum').textContent;
+    var wallet = document.querySelector('wallet').nodeValue;
+    var sum = document.querySelector('sum').value;
     let message = sum + ":" + wallet;
     answerWebAppQuery(message);
  });
